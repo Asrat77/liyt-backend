02b7e846f5a2ed03953de331f23d03630fd4dadf
Asrat <asratextras77@gmail.com>
2026-02-09 14:48:36 +0300

    fix: correct domain initializer paths

--- apps/liyt_api/config/initializers/liyt.rb
+++ apps/liyt_api/config/initializers/liyt.rb
@@ -1,6 +1,6 @@
-require_relative "../../../infra/lib/infra"
-require_relative "../../../domains/configuration"
-require_relative "../../../domains/delivery/registration"
+require_relative "../../../../infra/lib/infra"
+require_relative "../../../../domains/configuration"
+require_relative "../../../../domains/delivery/registration"
 
 Liyt::Configuration.new(
   registrations: [
